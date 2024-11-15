086026da5e5dc01f2b4c15522dc9ee75820a0fed
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-11-14 23:58:33 -0300

    prototipo banco

--- src/controllers/youtube_controller.py
+++ src/controllers/youtube_controller.py
@@ -1,5 +1,6 @@
 from model.youtube.youtube_service import YoutubeService
 from model.agente_ia.iiagente_ia import IIaagente
+from model.banco.db import SessionLocal
 
 
 class YoutubeController:
@@ -7,9 +8,10 @@ class YoutubeController:
     def __init__(self, ia_agente: IIaagente):
         self.__youtube = YoutubeService()
         self.__ia_agente = ia_agente
+        self.__database = SessionLocal()
 
     def obter_videos(self):
-        pass
+        self.__database.
 
     def gravar_videos(self):
         pass
